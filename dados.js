98779cb6bf950dd5763dc007a7c7c4bab2ff4b4f
KleberNevesTrindade <kleber.n.trindade@gmail.com>
2024-09-05 05:11:07 -0300

    v-03

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou um dos maiores nomes do skate mundial.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira. Sua trajetória é marcada por superação, talento e garra, conquistando o coração de milhões de fãs.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nas Olimpíadas de Paris. Sua história de superação e dedicação é inspiradora para muitos jovens atletas.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    }
+];
